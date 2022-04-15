ac2ef8cd77ce2dddfbe9eebec7b7aca59eab7d4b
Esron <esron.dtamar@gmail.com>
2022-04-15 15:38:36 -0300

    Add upload csv route

--- routes/web.php
+++ routes/web.php
@@ -1,5 +1,6 @@
 <?php
 
+use App\Http\Controllers\FileUploadController;
 use Illuminate\Support\Facades\Route;
 
 /*
@@ -16,3 +17,5 @@ use Illuminate\Support\Facades\Route;
 Route::get('/', function () {
     return view('welcome');
 });
+
+Route::post('/csv_upload', [FileUploadController::class, 'store'])->name('csv.upload');
