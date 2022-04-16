1d26366bd38e9ab7fca29b873294f9a2fca2bc9a
Esron <esron.dtamar@gmail.com>
2022-04-16 19:59:57 -0300

    Add csv file validation that don't need database

--- app/Http/Requests/CSVUploadRequest.php
+++ app/Http/Requests/CSVUploadRequest.php
@@ -3,6 +3,10 @@
 namespace App\Http\Requests;
 
 use Illuminate\Foundation\Http\FormRequest;
+use Illuminate\Support\Carbon;
+use Carbon\Exceptions\InvalidFormatException;
+use Illuminate\Support\Facades\Log;
+use Illuminate\Validation\Validator;
 
 class CSVUploadRequest extends FormRequest
 {
@@ -26,5 +30,79 @@ class CSVUploadRequest extends FormRequest
         return [
             'file' => 'required|file|mimes:txt,csv|max:2048',
         ];
+    }
+
+    /**
+     * Configure the validator instance.
+     *
+     * @return void
+     */
+    public function withValidator(Validator $validator)
+    {
+        $validator->after(function (Validator $validator) {
+            if ($this->file === null) {
+                return false;
+            }
+            $name = $this->file->getClientOriginalName();
+            $validationResponse = $this->validateCSV($this->file->path(), $validator);
+            if ($validationResponse === false) {
+                $validator->errors()->add('file', "Failed processing $name");
+                return;
+            }
+            $this->merge(['file' => $validationResponse]);
+        });
+    }
+
+    /**
+     * Return the csv file as an array or false if it fails to.
+     * Adds the validation errors to $validator
+     */
+    private function validateCSV(string $path, Validator $validator): array|bool
+    {
+        if (($handler = fopen($path, 'r')) === false) {
+            Log::error("Failed to open file in $path");
+            return false;
+        }
+        if (($firstLine = fgetcsv($handler, 1000)) === false) {
+            return false;
+        }
+        $lineNumber = 1;
+        $date = $this->getDateFromLine($firstLine, $validator, $lineNumber);
+        if ($date === false) {
+            return false;
+        }
+        $this->merge(['date' => $date]);
+        $lines = [$firstLine];
+        while (($line = fgetcsv($handler, 1000)) !== false) {
+            $lineNumber++;
+            $date = $this->getDateFromLine($line, $validator, $lineNumber);
+            $isSameDay = $date->isSameDay($this->date);
+            if ($date === false) {
+                return false;
+            }
+            if ($this->isLineComplete($line) === false || $isSameDay === false) {
+                Log::debug("Request Date: $this->date, Ignoring:", $line);
+                continue;
+            }
+            $lines[] = $line;
+            Log::info(join(',', $line));
+        }
+        return $lines;
+    }
+
+    private function getDateFromLine(array $line, Validator $validator, int $lineNumber): Carbon|bool
+    {
+        try {
+            return Carbon::parse(last($line));
+        } catch (InvalidFormatException) {
+            Log::error("InvalidFormat for date");
+            $validator->errors()->add('file', "Invalid format for date on line $lineNumber");
+            return false;
+        }
+    }
+
+    private function isLineComplete(array $line): bool
+    {
+        return ((bool) array_search("", $line)) === false;
     }
 }
