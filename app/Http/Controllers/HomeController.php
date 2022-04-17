b65e995ea3dcc50e17718ead26ed61ee68e5a6aa
Esron <esron.dtamar@gmail.com>
2022-04-17 15:45:28 -0300

    Sends trasactions data to welcome view

--- app/Http/Controllers/HomeController.php
+++ app/Http/Controllers/HomeController.php
@@ -2,12 +2,14 @@
 
 namespace App\Http\Controllers;
 
+use App\Models\Transaction;
 use Illuminate\Http\Request;
 
 class HomeController extends Controller
 {
     public function index()
     {
-        return view('welcome');
+        $transactions = Transaction::with(['originAccount', 'destinyAccount', 'import'])->get();
+        return view('welcome', ['transactions' => $transactions]);
     }
 }
