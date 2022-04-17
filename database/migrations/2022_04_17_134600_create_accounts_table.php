ca6f983d13e9c463d41fd50af7a0915bda6f5f42
Esron <esron.dtamar@gmail.com>
2022-04-17 11:30:25 -0300

    Create account model and migration

---
+++ database/migrations/2022_04_17_134600_create_accounts_table.php
@@ -0,0 +1,34 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('accounts', function (Blueprint $table) {
+            $table->id();
+            $table->string('bank_name', 250);
+            $table->string('branch', 4);
+            $table->string('number', 30);
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('accounts');
+    }
+};
