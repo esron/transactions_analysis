dfb4f44448153364ea450636ab47d291475d6576
Esron <esron.dtamar@gmail.com>
2022-04-17 12:44:17 -0300

    Create import model and migration

---
+++ app/Models/Import.php
@@ -0,0 +1,17 @@
+<?php
+
+namespace App\Models;
+
+use App\Models\Transaction;
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+
+class Import extends Model
+{
+    use HasFactory;
+
+    public function transactions()
+    {
+        return $this->hasMany(Transaction::class);
+    }
+}
