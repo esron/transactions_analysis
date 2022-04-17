564c7206faa8a9150f9949ea43f27a422ca98e60
Esron <esron.dtamar@gmail.com>
2022-04-17 14:58:20 -0300

    Save valid transactions

--- app/Http/Controllers/FileUploadController.php
+++ app/Http/Controllers/FileUploadController.php
@@ -3,6 +3,10 @@
 namespace App\Http\Controllers;
 
 use App\Http\Requests\CSVUploadRequest;
+use App\Models\Account;
+use App\Models\Import;
+use App\Models\Transaction;
+use Illuminate\Support\Carbon;
 use Illuminate\Support\Facades\Log;
 
 class FileUploadController extends Controller
@@ -16,9 +20,38 @@ class FileUploadController extends Controller
     {
         $file = $request->validated()['file'];
         $csv_lines = $request->get('file');
+        $this->saveTransactions($csv_lines, $request->get('date'));
         $name = $file->getClientOriginalName();
         $sizeInMb = $this->convertBytesToMegaBytes($file->getSize());
         Log::info("File name: $name, file size $sizeInMb mb");
         return redirect('/')->with('message', "File $name successfully uploaded!");
+    }
+
+    private function saveTransactions(array $transactions, Carbon $date)
+    {
+        $import = Import::create([
+            'transactions_date' => $date,
+        ]);
+        foreach ($transactions as $transaction) {
+            $transaction = collect($transaction);
+            $origin_data = $transaction->take(3);
+            $origin_account = Account::firstOrCreate([
+                'bank_name' => $origin_data[0],
+                'branch' => $origin_data[1],
+                'number' => $origin_data[2],
+            ]);
+            $destiny_data = $transaction->slice(3, 3)->values();
+            $destiny_account = Account::firstOrCreate([
+                'bank_name' => $destiny_data[0],
+                'branch' => $destiny_data[1],
+                'number' => $destiny_data[2],
+            ]);
+            Transaction::create([
+                'origin_account_id' => $origin_account->id,
+                'destiny_account_id' => $destiny_account->id,
+                'import_id' => $import->id,
+                'amount' => (int) $transaction[6],
+            ]);
+        }
     }
 }
