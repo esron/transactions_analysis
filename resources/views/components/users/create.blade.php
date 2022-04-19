166cdb73da3c4728446ff3af0f2ca6b0481f9008
Esron <esron.dtamar@gmail.com>
2022-04-19 19:20:04 -0300

    Adding validation messages to template

--- resources/views/components/users/create.blade.php
+++ resources/views/components/users/create.blade.php
@@ -6,11 +6,18 @@
             <div class="row">
                 <div class="col-md-12">
                     <div class="mb-3">
-                        <input class="form-control" type="text" name="name" placeholder="Nome" id="name">
-                        <input class="form-control mt-3" type="text" name="email" placeholder="E-mail" id="email">
-                        @error('email')
-                        <div class="alert alert-danger mt-1 mb-1">{{ $message }}</div>
-                        @enderror
+                        <div class="input-group">
+                            <input class="form-control @error('name') is-invalid @enderror" type="text" name="name" placeholder="Nome" id="name" value="{{ old('name') }}">
+                            @error('name')
+                            <div class="invalid-feedback mt-1 mb-1">{{ $message ?? '' }}</div>
+                            @enderror
+                        </div>
+                        <div class="input-group">
+                            <input class="form-control mt-3 @error('email') is-invalid @enderror" type="text" name="email" placeholder="E-mail" id="email" value="{{ old('email') }}">
+                            @error('email')
+                            <div class="invalid-feedback mt-1 mb-1">{{ $message ?? ''}}</div>
+                            @enderror
+                        </div>
                     </div>
                 </div>
                 <div class="col-md-12">
