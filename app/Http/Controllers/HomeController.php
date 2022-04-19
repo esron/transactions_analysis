55f4655364298cd6a5d2b0fd487aa2272adb112c
Esron <esron.dtamar@gmail.com>
2022-04-18 22:10:55 -0300

    Return home template from HomeController

--- app/Http/Controllers/HomeController.php
+++ app/Http/Controllers/HomeController.php
@@ -10,6 +10,6 @@ class HomeController extends Controller
     public function index()
     {
         $transactions = Transaction::with(['originAccount', 'destinyAccount', 'import'])->get();
-        return view('welcome', ['transactions' => $transactions]);
+        return view('home', ['transactions' => $transactions]);
     }
 }
