e86c80f3558121f917448737d7920e7e08aee27e
Esron <esron.dtamar@gmail.com>
2022-04-20 21:03:35 -0300

    Return user edit view in user controller

--- app/Http/Controllers/UserController.php
+++ app/Http/Controllers/UserController.php
@@ -72,7 +72,7 @@ class UserController extends Controller
      */
     public function edit(User $user)
     {
-        //
+        return view('components.users.edit', ['user' => $user]);
     }
 
     /**
