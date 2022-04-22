5ba27a607082e75a3031913c09d4d363ab43b0b2
Esron <esron.dtamar@gmail.com>
2022-04-22 13:55:46 -0300

    Add meta data to layout

--- resources/views/components/layout.blade.php
+++ resources/views/components/layout.blade.php
@@ -1,8 +1,10 @@
 <!DOCTYPE html>
-<html>
+<html lang="pt-BR">
 
 <head>
     <title>{{ $title ?? 'Validação de transações' }}</title>
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta name="description" content="Transaction Analysis System">
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-1BmE4kWBq78iYhFldvKuhfTAU6auU8tT94WrHftjDbrCEXSU1oBoqyl2QvZ6jIW3" crossorigin="anonymous">
 </head>
 
