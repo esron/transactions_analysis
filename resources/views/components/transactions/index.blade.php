24b89daf2070dba4161419f63f9c71dc3ccc4922
Esron <esron.dtamar@gmail.com>
2022-04-22 13:56:12 -0300

    Relate labels to inputs to help accessability

--- resources/views/components/transactions/index.blade.php
+++ resources/views/components/transactions/index.blade.php
@@ -5,25 +5,26 @@
             <div class="card-body">
                 <form>
                     <div class="row mb-2">
-                        <label class="col-sm-2 col-form-label">Importado em</label>
+                        <label for="createdAt" class="col-sm-2 col-form-label">Importado em</label>
                         <div class="col-sm-3">
-                            <input type="text" value="{{ $import->created_at->format('d/m/Y H:i') }}" class="form-control" disabled>
+                            <input id="createdAt"type="text" value="{{ $import->created_at->format('d/m/Y H:i') }}" class="form-control" disabled>
                         </div>
                     </div>
                     <div class="row mb-2">
-                        <label class="col-sm-2 col-form-label">Importado por</label>
+                        <label for="importedBy" class="col-sm-2 col-form-label">Importado por</label>
                         <div class="col-sm-3">
-                            <input type="text" value="{{ $import->user->name }}" class="form-control" disabled>
+                            <input id="importedBy" type="text" value="{{ $import->user->name }}" class="form-control" disabled>
                         </div>
                     </div>
                     <div class="row mb-2">
-                        <label class="col-sm-2 col-form-label">Data das transações</label>
+                        <label for="importedAt" class="col-sm-2 col-form-label">Data das transações</label>
                         <div class="col-sm-3">
-                            <input type="text" value="{{ $import->transactions_date->format('d/m/Y') }}" class="form-control" disabled>
+                            <input id="importedAt" type="text" value="{{ $import->transactions_date->format('d/m/Y') }}" class="form-control" disabled>
                         </div>
                     </div>
                 </form>
             </div>
         </div>
+        <h2 class="text-center mt-4">TRANSAÇÕES IMPORTADAS</h2>
     </div>
 </x-layout>
