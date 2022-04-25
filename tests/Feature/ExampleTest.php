8cf54aa88d814aa5603a0efbecbed9f44c848a5b
Esron <esron.dtamar@gmail.com>
2022-04-25 20:52:33 -0300

    Test if base route works fine

--- tests/Feature/ExampleTest.php
+++ tests/Feature/ExampleTest.php
@@ -2,20 +2,23 @@
 
 namespace Tests\Feature;
 
+use App\Models\User;
 use Illuminate\Foundation\Testing\RefreshDatabase;
 use Tests\TestCase;
 
 class ExampleTest extends TestCase
 {
+    use RefreshDatabase;
+
     /**
      * A basic test example.
      *
      * @return void
      */
-    public function test_the_application_returns_a_successful_response()
+    public function testTheApplicationReturnsASuccessfulResponse()
     {
-        $response = $this->get('/');
-
+        $user  = User::factory()->create();
+        $response = $this->actingAs($user)->get('/');
         $response->assertStatus(200);
     }
 }
