dc953f8a49b11dac69be33dddf734b2601ceb2aa
Esron <esron.dtamar@gmail.com>
2022-04-28 22:11:06 -0300

    Test validation of a import date already imported

--- tests/Feature/app/Http/Controllers/FileUploadControllerTest.php
+++ tests/Feature/app/Http/Controllers/FileUploadControllerTest.php
@@ -2,16 +2,18 @@
 
 namespace Tests\Feature\app\Http\Controllers;
 
+use App\Models\Import;
 use App\Models\User;
 use Illuminate\Foundation\Testing\RefreshDatabase;
 use Illuminate\Foundation\Testing\WithFaker;
 use Illuminate\Http\UploadedFile;
+use Illuminate\Support\Carbon;
 use Illuminate\Support\Facades\Storage;
 use Tests\TestCase;
 
 class FileUploadControllerTest extends TestCase
 {
-    use RefreshDatabase;
+    use RefreshDatabase, WithFaker;
 
     protected User $user;
 
@@ -63,6 +65,27 @@ class FileUploadControllerTest extends TestCase
         $response->assertStatus(302)
             ->assertInvalid([
                 'file' => 'Invalid format for date on line 1'
+            ]);
+    }
+
+    public function testFileUploadValidationImportForThisDateAlreadyImported()
+    {
+        $disk = 'temp';
+        $fileName = 'example.csv';
+        $importDate = '2022-01-01';
+        $fileContent = "BANCO DO BRASIL,0001,00001-1,BANCO BRADESCO,0001,00001-1,8000,{$importDate}T07:30:00";
+        Import::factory()->create([
+            'transactions_date' => Carbon::parse($importDate),
+        ]);
+        Storage::fake($disk);
+        $file = UploadedFile::fake()->createWithContent($fileName, $fileContent);
+        $response = $this->actingAs($this->user)->post('/file-upload', [
+            'file' => $file,
+        ]);
+        Storage::disk($disk)->assertExists($file->hashName());
+        $response->assertStatus(302)
+            ->assertInvalid([
+                'file' => 'Transactions for 2022-01-01 already imported'
             ]);
     }
 }
