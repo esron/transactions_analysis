0e4f29346bb622fc346b1c5ff36abd83bdd0dacc
Esron <esron.dtamar@gmail.com>
2022-04-28 22:31:16 -0300

    Test if file upload creates models

--- tests/Feature/app/Http/Controllers/FileUploadControllerTest.php
+++ tests/Feature/app/Http/Controllers/FileUploadControllerTest.php
@@ -88,4 +88,38 @@ class FileUploadControllerTest extends TestCase
                 'file' => 'Transactions for 2022-01-01 already imported'
             ]);
     }
+
+    public function testFileUploadCreatesAccountsTransactionsAndImports()
+    {
+        $disk = 'temp';
+        $fileName = 'example.csv';
+        $fileContent = $this->stubCsvFileContent();
+        Storage::fake($disk);
+        $file = UploadedFile::fake()->createWithContent($fileName, $fileContent);
+        $response = $this->actingAs($this->user)->post('/file-upload', [
+            'file' => $file,
+        ]);
+        Storage::disk($disk)->assertExists($file->hashName());
+        $response->assertStatus(302)
+            ->assertValid();
+        $this->assertDatabaseHas('accounts', [
+            'bank_name' => 'BANCO DO BRASIL',
+            'branch' => '0001',
+            'number' => '00001-1',
+        ]);
+        $this->assertDatabaseHas('transactions', [
+            'amount' => '8000',
+        ]);
+        $this->assertDatabaseHas('imports', [
+            'transactions_date' => '2022-01-01 00:00:00',
+        ]);
+    }
+
+    private function stubCsvFileContent(): string
+    {
+        return <<<EOT
+BANCO DO BRASIL,0001,00001-1,BANCO BRADESCO,0001,00001-1,8000,2022-01-01T07:30:00
+BANCO SANTANDER,0001,00001-1,BANCO BRADESCO,0001,00001-1,210,2022-01-01T08:12:00
+EOT;
+    }
 }
