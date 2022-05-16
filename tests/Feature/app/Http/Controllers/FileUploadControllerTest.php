7e75f62c5e53cced6e011b16ec6823e7fa82f75b
Esron <esron.dtamar@gmail.com>
2022-05-16 19:00:56 -0300

    Test import of xml file

--- tests/Feature/app/Http/Controllers/FileUploadControllerTest.php
+++ tests/Feature/app/Http/Controllers/FileUploadControllerTest.php
@@ -49,7 +49,7 @@ class FileUploadControllerTest extends TestCase
         ]);
         $response->assertStatus(302)
             ->assertInvalid([
-                'file' => 'The file must be a file of type: txt, csv.'
+                'file' => 'The file must be a file of type: txt, csv, xml.'
             ]);
     }
 
@@ -89,7 +89,7 @@ class FileUploadControllerTest extends TestCase
             ]);
     }
 
-    public function testFileUploadCreatesAccountsTransactionsAndImports()
+    public function testCSVFileUploadCreatesAccountsTransactionsAndImports()
     {
         $disk = 'temp';
         $fileName = 'example.csv';
@@ -115,11 +115,73 @@ class FileUploadControllerTest extends TestCase
         ]);
     }
 
+    public function testXMLFileUploadCreatesAccountsTransactionsAndImports()
+    {
+        $disk = 'temp';
+        $fileName = 'example.xml';
+        $fileContent = $this->stubXmlFileContent();
+        Storage::fake($disk);
+        $file = UploadedFile::fake()->createWithContent($fileName, $fileContent);
+        $response = $this->actingAs($this->user)->post('/file-upload', [
+            'file' => $file,
+        ]);
+        Storage::disk($disk)->assertExists($file->hashName());
+        $response->assertStatus(302)
+            ->assertValid();
+        $this->assertDatabaseHas('accounts', [
+            'bank_name' => 'BANCO DO BRASIL',
+            'branch' => '0001',
+            'number' => '00001-1',
+        ]);
+        $this->assertDatabaseHas('transactions', [
+            'amount' => '8000',
+        ]);
+        $this->assertDatabaseHas('imports', [
+            'transactions_date' => '2022-01-02 00:00:00',
+        ]);
+    }
+
     private function stubCsvFileContent(): string
     {
         return <<<EOT
 BANCO DO BRASIL,0001,00001-1,BANCO BRADESCO,0001,00001-1,8000,2022-01-01T07:30:00
 BANCO SANTANDER,0001,00001-1,BANCO BRADESCO,0001,00001-1,210,2022-01-01T08:12:00
+EOT;
+    }
+
+    private function stubXmlFileContent(): string
+    {
+        return <<<EOT
+<transacoes>
+  <transacao>
+    <origem>
+      <banco>BANCO DO BRASIL</banco>
+      <agencia>0001</agencia>
+      <conta>00001-1</conta>
+    </origem>
+    <destino>
+      <banco>BANCO BRADESCO</banco>
+      <agencia>0001</agencia>
+      <conta>00001-1</conta>
+    </destino>
+    <valor>8000</valor>
+    <data>2022-01-02T07:30:00</data>
+  </transacao>
+  <transacao>
+    <origem>
+      <banco>BANCO SANTANDER</banco>
+      <agencia>0001</agencia>
+      <conta>00001-1</conta>
+    </origem>
+    <destino>
+      <banco>BANCO BRADESCO</banco>
+      <agencia>0001</agencia>
+      <conta>00001-1</conta>
+    </destino>
+    <valor>210</valor>
+    <data>2022-01-02T07:30:00</data>
+  </transacao>
+</transacoes>
 EOT;
     }
 }
