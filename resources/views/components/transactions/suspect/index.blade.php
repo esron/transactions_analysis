27fd5a57b3adba3d5cde800e2b262ebaaa7daeb7
Esron <esron.dtamar@gmail.com>
2022-05-16 22:14:29 -0300

    Create index for suspect transactions

---
+++ resources/views/components/transactions/suspect/index.blade.php
@@ -0,0 +1,9 @@
+<x-layout>
+    <div class="container mt-4">
+        <h1 class="text-center">ANÁLISE DE TRANSAÇÕES SUSPEITAS</h1>
+        <form>
+            <label for="month">Selecione o mês para analisar as transações</label>
+            <input id="month" type="text" value="" class="form-control">
+        </form>
+    </div>
+</x-layout>
